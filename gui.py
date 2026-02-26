7e44ccbbfbf598f1f81e5d9db30541d779823b3d
badayaksir-svg <badayaksir@gmail.com>
2026-02-27 00:47:51 +0545

    added gui.py and imported libraries

---
+++ gui.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Wi-Fi Tool – GUI (fully self-contained & debugged version 2025)
+"""
+
+
+import tkinter as tk
+from tkinter import ttk, scrolledtext, messagebox, simpledialog
+import threading
+import queue
+import platform
+import sys
+import os
+import time
+import subprocess
+import ctypes
+
+

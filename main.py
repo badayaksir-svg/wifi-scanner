f3b9b3f7dc1b4865413e7ad281163e8a23866d76
badayaksir-svg <badayaksir@gmail.com>
2026-02-26 10:25:20 -0800

    a importing modules

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Wi-Fi Scanner + Handshake Capture + rockyou Dictionary Attack (console version)
+"""
+
+import os
+import sys
+import subprocess
+import time
+import platform
+import threading
+import queue
+import ctypes
